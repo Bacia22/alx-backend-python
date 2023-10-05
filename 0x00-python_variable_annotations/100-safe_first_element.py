ee0695cca5a47801e66d42c8c372121048f051b0
Bacia22 <adolfobacia@yahoo.com>
2023-10-05 20:07:21 +0000

    100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 """
-Duck typing sequence Any
+    Duck typing sequence Any
 """
 from typing import Any, Sequence, Union
 
 
-#The types of the elements of the input are not know
-def safe_first_element(1st: Sequence[Any]) -> Union[Any, None]:
+# The types of the elements of the input are not know
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
-    Args:
-    1st: Any data type
+        Args:
+            lst: Any data type
 
-    Return:
-    None or first element
+        Return:
+            None or first element
     """
-    if 1st:
-        return 1st[0]
+    if lst:
+        return lst[0]
     else:
         return None
