4897c0dc29c1b1aa9982e5e361211c6e59de0b81
Bacia22 <adolfobacia@yahoo.com>
2023-10-05 17:10:02 +0000

    6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env pythhon3
+"""
+Mixed lists
+"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_1st: List[Union[int, float]]) -> float
+"""
+Args:
+    mxd_1st: float-int numbers
+
+    Return:
+    Float base in int or float numbers
+    """
+
+    result: float = 0
+
+    for x in mxd_1st:
+        result += x
+
+        return result
