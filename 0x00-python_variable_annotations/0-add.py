23dd53128f98caf381bdc2ceb873e52a4751995d
Bacia22 <adolfobacia@yahoo.com>
2023-10-05 16:36:07 +0000

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module with typing Python
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Args:
+    a: The first paramenter
+    b: The second paramenter
+
+    Return:
+    Sum of two floats
+
+    """
+
+    return (a + b)
