63759a945ec270d9030bc0e2c8693b4b62d244bc
Bacia22 <adolfobacia@yahoo.com>
2023-10-05 16:48:27 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Conver float to integer
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+    n: float number
+
+    Return:
+    Float to type string
+    """
+
+    return str(n)
