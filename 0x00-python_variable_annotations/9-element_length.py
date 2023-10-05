efd109e6f2e20dfc9dc8911eb7b3a1cc1d9441eb
Bacia22 <adolfobacia@yahoo.com>
2023-10-05 19:40:24 +0000

    9-element_length.py

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """
-Duck type and iteration
+    Duck type and iteration
 """
 from typing import Iterable, Sequence, List, Union, Tuple
 
 
-def element_length(1st: Iterable[Sequence])\
+def element_length(lst: Iterable[Sequence])\
         -> List[Tuple[Sequence, int]]:
-            """
-            Args:
-            1st: Sequence of list
+    """
+        Args:
+            lst: Sequence of list
 
-            Return:
+        Return:
             List of tuple of sequence of integers
-            """
+    """
 
-            return [(i, len(1) for i in 1st]
+    return [(i, len(i)) for i in lst]
