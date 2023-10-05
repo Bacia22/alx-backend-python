8ed86488a6b367a0961c5741b6116d610f614b78
Bacia22 <adolfobacia@yahoo.com>
2023-10-05 18:00:01 +0000

    7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Mixed Tuple
+"""
+from typing import union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]"
+"""
+Args:
+    k: Dtring
+    v: Union: Can be int or float
+
+    Return:
+        Tuple with string and int or float
+        """
+
+        cncat: Tuple(str, Union[int, float])
+        cncat = (k, v**2)
+
+        return cncat
